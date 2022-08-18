123aab52e7175fb1cec885669f42432a360aa0e8
Jérôme Maurin <j@jmaurin.ovh>
2022-08-18 17:37:06 +0200

    PS1 : Added support for auto-renaming of Terminal Window and debian_chroot & added ~/bin in PATH

--- environment.bash
+++ environment.bash
@@ -7,7 +7,11 @@ then
   GIT_PS1_SHOWUPSTREAM="verbose name"
   GIT_PS1_DESCRIBE_STYLE=branch
   GIT_PS1_SHOWCOLORHINTS=true
-  PROMPT_COMMAND='__git_ps1 "" "\[\e[01;34m\][\t]\[\e[00m\]#\[\e[01;35m\]\u\[\e[00m\]@\[\e[01;32m\]\h\[\e[00m\]:\[\e[01;36m\]\w\[\e[00m\]$ " "%s\n"'
+  PROMPT_COMMAND='__git_ps1 "" "\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\e[01;34m\][\t]\[\e[00m\]#\[\e[01;35m\]\u\[\e[00m\]@\[\e[01;32m\]\h\[\e[00m\]:\[\e[01;36m\]\w\[\e[00m\]$ " "%s\n"'
 else
-  PS1='\[\e[01;34m\][\t]\[\e[00m\]#\[\e[01;35m\]\u\[\e[00m\]@\[\e[01;32m\]\h\[\e[00m\]:\[\e[01;36m\]\w\[\e[00m\]$ '
+  PS1='\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\e[01;34m\][\t]\[\e[00m\]#\[\e[01;35m\]\u\[\e[00m\]@\[\e[01;32m\]\h\[\e[00m\]:\[\e[01;36m\]\w\[\e[00m\]$ '
+fi
+if [ -d ~/bin ]
+then
+  PATH=$PATH:~/bin
 fi
