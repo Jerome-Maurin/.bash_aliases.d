343d6938788ff96cedbc3aedf50763898f7410b0
Jérôme Maurin <j@jmaurin.ovh>
2023-07-27 21:17:35 +0200

    Reworked PS1 code to support late rcfile PS1 changes being taken into account when the git PS1 support is enabled

--- environment.bash
+++ environment.bash
@@ -1,4 +1,6 @@
-#PS1 or advanced git version customization
+PS1='\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\e[01;34m\][\t]\[\e[00m\]#\[\e[01;35m\]\u\[\e[00m\]@\[\e[01;32m\]\h\[\e[00m\]:\[\e[01;36m\]\w\[\e[00m\]$ '
+# If git is present on the system
+# enable the advanced git version customization
 if type git > /dev/null 2>&1
 then
   GIT_PS1_SHOWDIRTYSTATE=true
@@ -7,9 +9,11 @@ then
   GIT_PS1_SHOWUPSTREAM="verbose name"
   GIT_PS1_DESCRIBE_STYLE=branch
   GIT_PS1_SHOWCOLORHINTS=true
-  PROMPT_COMMAND='__git_ps1 "" "\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\e[01;34m\][\t]\[\e[00m\]#\[\e[01;35m\]\u\[\e[00m\]@\[\e[01;32m\]\h\[\e[00m\]:\[\e[01;36m\]\w\[\e[00m\]$ " "%s\n"'
-else
-  PS1='\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\e[01;34m\][\t]\[\e[00m\]#\[\e[01;35m\]\u\[\e[00m\]@\[\e[01;32m\]\h\[\e[00m\]:\[\e[01;36m\]\w\[\e[00m\]$ '
+  __pc_first_time=true
+  # The use of the _if_ directive and the PS1BASE variable
+  # makes it possible to take into account a potential PS1 change
+  # done after this scipt is called
+  PROMPT_COMMAND='if $__pc_first_time; then PS1BASE=$PS1; __pc_first_time=false; fi; __git_ps1 "" "$PS1BASE" "%s\n"'
 fi
 if [ -d ~/bin ]
 then
