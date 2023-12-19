019d9cc2b2e586798ab273ec600f96a05ed4eaa4
Jérôme Maurin <j@jmaurin.ovh>
2023-12-19 17:55:43 +0100

    Reworked lssh : now supports ssh-agent -a option

--- ssh-agent.bash
+++ ssh-agent.bash
@@ -1,7 +1,7 @@
 # Works only with BASH
 load_ssh-agent()
 {
-  if ! agents=($(pgrep ssh-agent))
+  if ! agents=($(pgrep -x ssh-agent))
   then
     echo "No ssh-agent instance detected."
     echo "Starting one."
@@ -11,13 +11,31 @@ load_ssh-agent()
     if [ ${#agents[@]} -gt 1 ];
     then
       echo "More than one ssh-agent instance are already running !"
-      echo "Please clean-up your system manualy. Aborting ..."
+      echo "Please clean-up your system manually. Aborting ..."
       return 1
     fi
   fi
 
-  SSH_AUTH_SOCK=$(echo /tmp/ssh-*/agent.*); export SSH_AUTH_SOCK
-  SSH_AGENT_PID=$(pgrep ssh-agent); export SSH_AGENT_PID
+  if cmdlne=($(pgrep -x ssh-agent -a | grep -- \ -a))
+  then
+    for ((n=0; n<${#cmdlne[@]}; n++))
+    do
+      if [ "${cmdlne[$n]}" = "-a" ]
+      then
+        if [ $n -lt $((${#cmdlne[@]} - 1)) ]
+        then
+          export SSH_AUTH_SOCK="${cmdlne[$(($n + 1))]}"
+        else
+          echo "Wrong parameters for ssh-agent found !"
+        fi
+        break
+      fi
+    done
+  else
+    export SSH_AUTH_SOCK=$(compgen -G /tmp/ssh-*/agent.*)
+  fi
+
+  export SSH_AGENT_PID=$agents
 
   echo "List of active keys :"
 
