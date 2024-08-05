4800a6833d3e29eb504f6c128852c264afa3f58e
Jérôme Maurin <j@jmaurin.ovh>
2024-08-05 12:43:40 +0200

    Reworking PS1 : Changed window/term title
    With a shorter title the size of which is not modified on current directory change,
    the use of tabs in a graphical terminal emulator (Konsole for example) offers a better user experience

--- environment.bash
+++ environment.bash
@@ -1,4 +1,4 @@
-PS1='\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\e[01;34m\][\t]\[\e[00m\]#\[\e[01;35m\]\u\[\e[00m\]@\[\e[01;32m\]\h\[\e[00m\]:\[\e[01;36m\]\w\[\e[00m\]$ '
+PS1='\[\e]0;\u@\h\]${debian_chroot:+($debian_chroot)}\[\e[01;34m\][\t]\[\e[00m\]#\[\e[01;35m\]\u\[\e[00m\]@\[\e[01;32m\]\h\[\e[00m\]:\[\e[01;36m\]\w\[\e[00m\]$ '
 # If git is present on the system
 # enable the advanced git version customization
 if type git > /dev/null 2>&1
